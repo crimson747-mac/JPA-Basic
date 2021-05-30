75f62db5ddec3bc2a5c9179e835ebc2196e7ef61
김남우 <crimson747@naver.com>
2021-05-30 20:42:19 +0900

    도메인 분석 설계

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +14,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(false)
-    void testMember() throws Exception {
-        // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        // then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-    }
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    void testMember() throws Exception {
+//        // given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        // when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        // then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//    }
 }
